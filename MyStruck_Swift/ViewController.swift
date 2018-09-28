dbeea77bb27cdb6c01a8e84f390aedeeb65c6c64
AidyBao <aidybao555@sina.com>
2018-09-28 16:53:00 +0800

    Initial Commit

---
+++ MyStruck_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyStruck_Swift
+//
+//  Created by 120v on 2018/9/28.
+//  Copyright © 2018 MQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
